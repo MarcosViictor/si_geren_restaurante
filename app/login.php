0c60e46ae0261ec1eeb4b1cd404d91f519324df0
ravel fonseca <ravelfonseca@gmail.com>
2024-05-10 21:21:51 -0300

    tela de login - Ravel

--- app/login.php
+++ app/login.php
@@ -3,16 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../app/css/login.css">
+    
     <title>Document</title>
 </head>
 <body>
     <form action="login_usuario.php" method="post">
-        <input type="text" name="email" placeholder="email">
-        <br><br>
-        <input type="password" name="senha" placeholder="senha">
-        <br><br>
-        <input type="submit" value="enviar" name="submit">
+    <div class="login">
+            <h1>Entrar na sua conta</h1>
 
+            <input type="email" placeholder="Email" name="email">
+            <input type="password" placeholder="Senha" name="senha">
+
+            <button>Entrar</button>
+        </div>
+            
+        <div class="adm">
+            <h5>Entrar como administrador</h5>
+        </div>
+
+        <div class="imagem">
+
+        <figure class="google">
+            <img src="/img/google-icon.png" alt="">
+        </figure>
+
+        <figure class="face">
+            <img src="/img/facebook-logo-5-1.png" alt="">
+        </figure>   </div>
+
+    </div>
         
     </form>
 </body>
